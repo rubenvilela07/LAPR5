72db2b2b2cd98f38447c18bfac0aa8cab6a378a4
Ruben Vilela <1211861@isep.ipp.pt>
2024-07-01 01:53:51 +0100

    passage created

--- src/repos/passageRepo.ts
+++ src/repos/passageRepo.ts
@@ -21,9 +21,41 @@ export default class PassageRepo implements IPassageRepo {
     const passageDocument = await this.passageSchema.findOne(query);
     return !!passageDocument === true;
   }
-  save(t: Passage): Promise<Passage> {
-    throw new Error("Method not implemented.");
+
+  public async findByDomainId(id: PassageId): Promise<Passage> {
+    const query = { domainId: id.toString() };
+    const passageRecord = await this.passageSchema.findOne
+    (query) as IPassagePersistence & Document;
+    if (passageRecord == null) return null;
+    return PassageMap.toDomain(passageRecord);
   }
+
+  public async save(t: Passage): Promise<Passage> {
+    const query = { domainId: t.id.toString() };
+    const passageDocument = await this.passageSchema.findOne(query);
+    try {
+      if (passageDocument === null) {
+        const rawPassage: any = PassageMap.toPersistence(t);
+        const passageCreated = await this.passageSchema.create(rawPassage);
+        return PassageMap.toDomain(passageCreated);
+      } else {
+        passageDocument.building1_x = t.building1_x;
+        passageDocument.building1_y = t.building1_y;
+        passageDocument.building2_x = t.building2_x;
+        passageDocument.building2_y = t.building2_y;
+        passageDocument.floor = t.floor;
+        passageDocument.buildingCode1 = t.buildingCode1.code;
+        passageDocument.buildingCode2 = t.buildingCode2.code;
+        
+        await passageDocument.save();
+        return PassageMap.toDomain(passageDocument);
+      }
+    } catch (err) {
+      throw err;
+    }
+  }
+
+
 
 
 }
--- src/services/passageService.ts
+++ src/services/passageService.ts
@@ -9,6 +9,7 @@ import IPassageDTO from "../dto/IPassageDTO";
 import { Passage } from "../domain/passage/passage";
 import { PassageMap } from "../mappers/PassageMap";
 import IRoleRepo from "../repos//IRepos/IRoleRepo";
+import { Building } from "../domain/building/building";
 
 
 @Service()
@@ -17,22 +18,82 @@ export default class PassageService implements IPassageService {
     @Inject(config.repos.passage.name) private passageRepo: IPassageRepo,
     @Inject(config.repos.floor.name) private floorRepo: IFloorRepo,
     @Inject(config.repos.building.name) private buildingRepo: IBuildingRepo,
-    @Inject(config.repos.role.name) private roleRepo: IRoleRepo
   ) {}
 
   public async createPassage(passageDTO: IPassageDTO): Promise<Result<IPassageDTO>> {
     try {
-      throw new Error("Method not implemented.");
+      const building1  = await this.buildingRepo.findByCode(passageDTO.buildingCode1);
+      if (!building1) {
+        return Result.fail<IPassageDTO>("Building 1 not found");
+      }
 
+      const building2  = await this.buildingRepo.findByCode(passageDTO.buildingCode2);
+      if (!building2) {
+        return Result.fail<IPassageDTO>("Building 2 not found");
+      }
+
+      if (building1.id.toString() === building2.id.toString()) {
+        return Result.fail<IPassageDTO>("Buildings must be different");
+      }
+
+      if(!(this.validatePassagePosition(passageDTO.building1_x, passageDTO.building1_y, building1))){
+        return Result.fail<IPassageDTO>("Invalid position for building 1");
+      }
+
+      if(!(this.validatePassagePosition(passageDTO.building2_x, passageDTO.building2_y, building2))){
+        return Result.fail<IPassageDTO>("Invalid position for building 2");
+      }
+
+      if(building1.numberOfFloors < passageDTO.floor || building2.numberOfFloors < passageDTO.floor){
+        return Result.fail<IPassageDTO>("Floor does not exist in one of the buildings");
+      }
+
+      const passageOrError = await Passage.create(passageDTO);
+      if (passageOrError.isFailure) {
+        return Result.fail<IPassageDTO>(passageOrError.error);
+      }
+
+      const passage = passageOrError.getValue();
+
+      const passageExists = await this.passageRepo.exists(passage);
+      if (passageExists) {
+        return Result.fail<IPassageDTO>("Passage already exists");
+      }
+
+      await this.passageRepo.save(passage);
+
+      const passageDTOresult = PassageMap.toDTO(passage) as IPassageDTO;
+      return Result.ok<IPassageDTO>(passageDTOresult);
     } catch (e) {
       return Result.fail<IPassageDTO>(e);
     }
   }
+
   updatePassage(passageDTO: IPassageDTO): Promise<Result<IPassageDTO>> {
     throw new Error("Method not implemented.");
   }
   getAllPassages(): Promise<Result<IPassageDTO[]>> {
     throw new Error("Method not implemented.");
+  }
+
+  public validatePassagePosition(x: number, y: number, building: Building): boolean {
+    if (x < 0 || x > building.dimension.width) {
+      return false;
+    }
+
+    if (y < 0 || y > building.dimension.length) {
+      return false;
+    }
+
+    if (x == 0 && y <= building.dimension.length || x == building.dimension.width && y <= building.dimension.length) {
+      return true;
+    }
+
+    if (y == 0 && x <= building.dimension.width || y == building.dimension.length && x <= building.dimension.width) {
+      return true;
+    }
+
+    return false;
   }
 
   
